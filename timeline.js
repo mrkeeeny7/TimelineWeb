19eaafb22b2a06a5c060d574de8f9719a6d98e5e
mrkeeeny7 <ken.catterall@gmail.com>
2021-01-15 11:06:48 +1000

    Added initial working JSON parser

--- timeline.js
+++ timeline.js
@@ -1,11 +1,27 @@
 
 function myfunc()
 {
-
-    var jsonData = JSON.parse()
-
     document.getElementById("mainTable")
     .innerHTML = "Testing";
+}
+
+function parseJSON()
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            var eventsString = "";
+            eventsString += myObj.category + ": ";
+            for(var i=0; i<myObj.eventlist.length; i++)
+            {
+                eventsString += myObj.eventlist[i].title + ", ";
+            }
+            document.getElementById("mainTable").innerHTML = eventsString;
+        }
+    };
+    xmlhttp.open("GET", "timelines/timeline.json", true);
+    xmlhttp.send();
 }
 
 
